24e6ce53393e2991a688c135b0383bf0ec35ea2f
Wahyu Untoro <127081937+wahyuunt97@users.noreply.github.com>
2025-01-26 03:15:21 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,38 +1,19 @@
 // create web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
+const express = require('express');
+const app = express();
 
-// create a server
-http.createServer(function (req, res) {
-    // parse the query string
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
+// create a comment object
+const comment = {
+    name: 'John Doe',
+    comment: 'This is a test comment'
+};
 
-    // read the file content
-    var data = fs.readFileSync('comments.json');
-    var comments = JSON.parse(data);
+// create a route for the comment object
+app.get('/comment', (req, res) => {
+    res.json(comment);
+});
 
-    // if the request is a POST request
-    if (req.method == 'POST') {
-        var body = '';
-
-        req.on('data', function (data) {
-            body += data;
-        });
-
-        req.on('end', function () {
-            var newComment = qs.parse(body);
-            comments.push(newComment);
-
-            fs.writeFileSync('comments.json', JSON.stringify(comments));
-
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Comment added');
-        });
-    } else {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(comments));
-    }
-}).listen(8080, '127.0.0.1');
+// start the web server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
